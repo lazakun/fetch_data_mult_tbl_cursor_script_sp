01e0cb73d055e7db79210640f7a523aa34fd6ef0
lazakun <adegbenro.yinka@gmail.com>
2024-01-09 18:03:45 +0100

    initial commit

---
+++ using_sp/fetch_script.sql
@@ -0,0 +1,41 @@
+CREATE TABLE #my_dest_table (
+	[BusinessEntityID] [int] NOT NULL,
+	[NationalIDNumber] [nvarchar](15) NOT NULL,
+	[LoginID] [nvarchar](256) NOT NULL,
+	[OrganizationNode] [hierarchyid] NULL,
+	[OrganizationLevel] [smallint] NULL,
+	[JobTitle] [nvarchar](50) NOT NULL,
+	[BirthDate] [date] NOT NULL,
+	[MaritalStatus] [nchar](1) NOT NULL,
+	[Gender] [nchar](1) NOT NULL,
+	[HireDate] [date] NOT NULL,
+	[SalariedFlag] [smallint] NOT NULL,
+	[VacationHours] [smallint] NOT NULL,
+	[SickLeaveHours] [smallint] NOT NULL,
+	[CurrentFlag] [smallint] NOT NULL,
+	[rowguid] [uniqueidentifier] NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+	[table_name] [nvarchar](500) NULL);
+
+
+
+DECLARE  @table_name VARCHAR(MAX) 
+DECLARE cursor_product CURSOR
+FOR  
+select name as table_name from sys.tables
+where name in ('Employee_Buyer','Employee_Janitor','Employee_Prod_Tech','Employee_Sales_Rep')
+  OPEN cursor_product;
+FETCH NEXT FROM 
+  cursor_product INTO         
+  @table_name
+  WHILE @@FETCH_STATUS = 0    
+  BEGIN 
+exec('insert into #my_dest_table
+exec [dbo].[fetch_script] '+@table_name)
+	FETCH NEXT FROM cursor_product INTO
+	@table_name
+	END;
+	CLOSE cursor_product;
+DEALLOCATE cursor_product; 
+
+--select * from #my_dest_table
